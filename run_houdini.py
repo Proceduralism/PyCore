6401d90786a4418a675dc4f588f9317453926292
Jihyun Nam <initialfx@gmail.com>
2021-01-16 22:26:11 -0800

    update run_houdini.py

--- run_houdini.py
+++ run_houdini.py
@@ -1,17 +1,14 @@
 # Wrapper to run Houdini with custom environment
 import os, subprocess, sys, json
+from SetShotCore import SetShot
+
+Ss = SetShot()
 
 #sys.path.append("C:/Program Files/Side Effects Software/Houdini 18.0.287/houdini/python2.7libs")
 
 houdini = 'C:/Program Files/Side Effects Software/Houdini 18.5.351/bin/houdini.exe'
 
 os.environ['HOME'] = '{}'.format("J:/HOU")
-
-def loadJson(json_file):
-    with open(json_file) as data_file:
-        data = json.load(data_file)
-        #data = [s.encode('utf-8') for s in data_file]        
-    return data
 
 env_json = '{}/{}'.format(os.getenv("HOME")
                      , "env.json")
@@ -21,9 +18,9 @@ env_json = '{}/{}'.format(os.getenv("HOME")
 
 
 os.environ['PROJ'] = '{}'.format("//fx8/hq/proj")
-os.environ['SHOW'] = '{}'.format(loadJson(env_json)["envs"][0])
-os.environ['SEQ'] = '{}'.format(loadJson(env_json)["envs"][1])
-os.environ['SHOT'] = '{}'.format(loadJson(env_json)["envs"][2])
+os.environ['SHOW'] = '{}'.format(Ss.loadJson(env_json)["envs"][0])
+os.environ['SEQ'] = '{}'.format(Ss.loadJson(env_json)["envs"][1])
+os.environ['SHOT'] = '{}'.format(Ss.loadJson(env_json)["envs"][2])
 
 # Add Arnold to Path
 os.environ['PATH'] = '$PATH;{}'.format("C:/htoa/htoa-5.0.1_r5e954ab_houdini-18.0.287/htoa-5.0.1_r5e954ab_houdini-18.0.287/scripts/bin")
@@ -34,6 +31,4 @@ if __name__ == "__main__":
     
     # Run Houdini
     subprocess.Popen(houdini)
-
-
 
